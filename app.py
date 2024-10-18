e830f79d49189194e5e4861021f132f762054073
Samuka-vitor <samuelvitor93andrade@gmail.com>
2024-10-18 19:20:26 -0300

    versão 1.0

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 # Importar biblioteca de automação 
+# instalação do cx Freeze: pip install cx_Freeze
+# comando para gerar executável: cxfreeze app.py --target-fir nome-pasta
+
 import pyautogui as auto 
 
 auto.PAUSE = 0.7 
@@ -6,6 +9,8 @@ auto.PAUSE = 0.7
 auto.press("win")
 auto.write("edge")
 auto.press("enter")
+auto.hotkey("alt", "sapce")
+auto.press("x")
 auto.write("python.org")
 auto.press("enter")
 auto.hotkey("ctrl","t")
